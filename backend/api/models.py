92112bda12b4c7b0bacc5c7978b62cd290d2c0cd
Suyash Mullick <suyash.mullick@gmail.com>
2025-05-19 22:04:38 +0200

    Added venv and .env, created User and Task models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "male", "Male"
+        FEMALE = "female", "Female"
+        OTHER = "other", "Other"
+    gender = models.CharField(max_length=10)
+    points = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.username
 
 class Task(models.Model):
+    id = models.UUIDField()
     name = models.CharField(max_length=255)
     description = models.TextField()
     points = models.IntegerField(10)
