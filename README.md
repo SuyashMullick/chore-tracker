5d535f8ab6f27d4df8406484f8e24415917dd764
Wenyu <wh222em@student.lnu.se>
2025-05-20 10:59:48 +0200

    Add instruction to README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # README
-To be done
+
+## To use Django:
+1. 'cd equalchores/dev_django'
+2. activate virtual environment by 'python -m venv venv' and '\venv\Scripts\activate'      
+# source venv/bin/activate # Mac/Linux
+3. 'pip install -r requirements.txt'
+4. Run the Development Server
+'python manage.py runserver'
+
+Note:
+1. Always activate the virtual environment before running any commands.
+2. Communicate any changes to requirements.txt to the team.
+
+
+## To use SQLite: 
+'python manage.py makemigrations'
+'python manage.py migrate'
+'python manage.py shell' # open shell
+'from api.models import User'
+# Save new data
+p = User(points="4", gender="Female")
+p.save()
+# inquiry data
+users = User.objects.all()
+print(users)
+
+sqlite3 db.sqlite3	
+
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,23 +1,29 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 
 class User(AbstractUser):
     class GenderChoices(models.TextChoices):
         MALE = "male", "Male"
         FEMALE = "female", "Female"
         OTHER = "other", "Other"
-    gender = models.CharField(max_length=10)
+
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+        default=GenderChoices.OTHER
+    )
     points = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.username
 
+
 class Task(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    points = models.IntegerField(10)
+    points = models.IntegerField(default=10)
 
     def __str__(self):
         return self.name
