53bb02d908ff9ec74b0d23ea41061a84a64ce99e
Wenyu <wh222em@student.lnu.se>
2025-05-22 15:39:40 +0200

    finished the user, group, and groupmembership in models

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,31 +6,43 @@ class User(AbstractUser):
     class GenderChoices(models.TextChoices):
         MALE = "male", "Male"
         FEMALE = "female", "Female"
-        OTHER = "other", "Other"
+        OTHER =  "other", "Other"
 
     gender = models.CharField(
         max_length=10,
         choices=GenderChoices.choices,
         default=GenderChoices.OTHER
     )
-    # points = models.IntegerField(default=0)
-    userId = models.models.CharField(max_length=50)
-    password = models.models.CharField(max_length=50)
-    email = models.models.CharField(max_length=50)
-
+    # user_id = models.CharField(max_length=50, primary_key=True)
 
     def __str__(self):
         return self.username
 
-class Group(AbstractUser):
-    group_id = models.models.CharField(max_length=50)
-    user_id = models.models.CharField(max_length=50)
-    team_credits = models.IntegerField()
-    individual_credits = models.IntegerField()
+class Group(models.Model):
+    # group_id = models.CharField(max_length=100, unique=True)
+    # there is a default id provided by Django
+    group_name = models.CharField(max_length=100)
+    users = models.ManyToManyField(User, related_name='groups')
+
+    def __str__(self):
+        return self.group_name
 
 
-class Task(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class GroupMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    team_credits = models.IntegerField(default=0)
+    individual_credits = models.IntegerField(default=0)
+    
+    class Meta:
+        unique_together = ('user', 'group') 
+    def __str__(self):
+        return f"{self.user.username} in {self.group.group_name}"
+
+
+class Task_created(models.Model):
+    # task_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # there is a default id provided by Django
     name = models.CharField(max_length=255)
     description = models.TextField()
     points = models.IntegerField(default=10)
