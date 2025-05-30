9300f5048868cdd9b4a7a06e14b7602bea360be4
Wenyu <wh222em@student.lnu.se>
2025-05-30 23:13:43 +0200

    1. View uses servece  instead of serializer now; 2. can create planned task at the backend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,6 @@ from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import User, Group, GroupMembership, CreatedTask, PlannedTask, User
-from .services import create_created_task
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -38,25 +37,7 @@ class TaskTemplateCreateSerializer(serializers.Serializer):
     task_name = serializers.CharField(max_length=100)
     description = serializers.CharField(required=False, allow_blank=True)
     points = serializers.IntegerField(min_value=1, max_value=10)
-
-    def create(self, validated_data):
-        return create_created_task(
-            group=validated_data["group"],
-            task_name=validated_data["task_name"],
-            description=validated_data.get("description", ""),
-            points=validated_data["points"],
-        )
-    # def validate(self, attrs):
-    #     group = attrs['group']
-    #     task_name = attrs['task_name']
-    #     if CreatedTask.objects.filter(group=group, task_name=task_name).exists():
-    #         raise ValidationError("Task with this name already exists in this group")
-    #     return attrs
-
-    # def create(self, validated_data):
-    #     return CreatedTask.objects.create(**validated_data)
     
-
 class PlannedTaskDisplaySerializer(ModelSerializer):
     task_template = TaskTemplateDisplaySerializer(read_only=True)
     assignee = UserSerializer(read_only=True)
@@ -72,13 +53,7 @@ class PlannedTaskCreateSerializer(serializers.Serializer):
     state = serializers.ChoiceField(choices=PlannedTask.StateChoices)
     custom_points = serializers.IntegerField(required=False, min_value=1, max_value=10)
     start_time = serializers.DateTimeField()
-    def validate(self, attrs):
-        if attrs['assignee'] == attrs['assigner']:
-            raise ValidationError("Assignee and assigner cannot be the same user.")
-        return attrs
 
-    def create(self, validated_data):
-        return PlannedTask.objects.create(**validated_data)
     
 class PlannedTaskUpdateSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,9 @@
 from rest_framework import viewsets
-from .serializers import UserSerializer, GroupSerializer, GroupMembershipSerializer
-from .serializers import TaskTemplateCreateSerializer, TaskTemplateDisplaySerializer, PlannedTaskDisplaySerializer, PlannedTaskCreateSerializer,PlannedTaskUpdateSerializer
-
-from rest_framework import status
+from .serializers import *
+from .models import *
+from .services import *
 from rest_framework.response import Response
-from .models import User, Group, GroupMembership, CreatedTask, PlannedTask
-
+from rest_framework import status
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -27,7 +25,24 @@ class TaskTemplateViewSet(viewsets.ModelViewSet):
         elif self.action in ["list", "retrieve"]:
             return TaskTemplateDisplaySerializer
         return TaskTemplateDisplaySerializer
-
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            try:
+                task_template = create_task_template(
+                    group=data["group"],
+                    task_name=data["task_name"],
+                    description=data.get("description", ""),
+                    points=data["points"]
+                )
+                output_serializer = TaskTemplateDisplaySerializer(task_template)
+                return Response(output_serializer.data, status=status.HTTP_201_CREATED)
+            except ValidationError as e:
+                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class PlannedTaskViewSet(viewsets.ModelViewSet):
     queryset = PlannedTask.objects.all()
     
@@ -39,9 +54,29 @@ class PlannedTaskViewSet(viewsets.ModelViewSet):
         elif self.action in ['update', 'partial_update']:
             return PlannedTaskUpdateSerializer
         return PlannedTaskDisplaySerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            try:
+                planned_task=PlannedTask(
+                    task_template=data["task_template"],
+                    assignee=data.get("assignee"),
+                    assigner=data["assigner"],
+                    start_time=data["start_time"],
+                    custom_points=data["custom_points"]
+                )
+                planned_task.save()
+                output_serializer = PlannedTaskDisplaySerializer(planned_task)
+                return Response(output_serializer.data, status=status.HTTP_201_CREATED)
+            except ValidationError as e:
+                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
         
-
     # def partial_update(self, request, *args, **kwargs):
     #     instance = self.get_object()
     #     new_state = request.data.get("state")
